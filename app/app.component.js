c2d776b692b25751bd891efcccec9d737302a0a0
Kal Zoghaibi <kzoghaibi@corp.ipipenet.com>
2017-02-27 16:36:20 -0500

    Starting convertion to certSnifferUI

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.pageTitle = "Acme Product Managment";
+        this.pageTitle = 'Tier III Support';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: "\n        <div>\n            <nav class='navbar navbar-default'>\n                <div class='container-fluid'>\n                    <a class='navbar-brand'>{{pageTitle}}</a>\n                    <ul class='nav navbar-nav'>\n                        <li><a [routerLink]=\"['/welcome']\">Home</a></li>\n                        <li><a [routerLink]=\"['/products']\">Product List</a></li>\n                    </ul>\n                </div>\n            </nav>\n            <div class='container'>\n                <router-outlet></router-outlet>\n            </div>\n        </div>\n    "
+        templateUrl: 'app/app.component.html'
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
